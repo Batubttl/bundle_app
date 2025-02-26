46eb42e22e3b7237958b35e719bf74c4d5be7358
Batuhan Battal <batubattal@gmail.com>
2025-02-26 13:14:15 +0300

    Currenct and weather screen added

---
+++ lib/presentation/views/drawer/weather_viewmodel.dart
@@ -0,0 +1,27 @@
+import 'package:bundle_app/services/weather_service.dart';
+import 'package:flutter/material.dart';
+
+class WeatherViewModel extends ChangeNotifier {
+  final WeatherService _weatherService;
+
+  WeatherViewModel(this._weatherService);
+
+  WeatherData? weatherData;
+  bool isLoading = false;
+  String? error;
+
+  Future<void> getWeatherDetails() async {
+    try {
+      isLoading = true;
+      error = null;
+      notifyListeners();
+
+      weatherData = await _weatherService.getWeatherDetails();
+    } catch (e) {
+      error = e.toString();
+    } finally {
+      isLoading = false;
+      notifyListeners();
+    }
+  }
+}
